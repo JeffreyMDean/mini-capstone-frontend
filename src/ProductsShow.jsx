1750206c32c4b9da17a7c8a1a8266a274cef995a
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-06-21 13:44:42 -0400

    added cart button to ProductsShow

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,4 +1,18 @@
 export function ProductsShow(props) {
+
+  const submit = (event) => {
+    event.preventDefault()
+    console.log('in submit')
+    const params = new FormData(event.target)
+
+
+    axios.post('http://localhost:3000/carted_products.json', 
+      params).then(response => {
+        console.log(response.data)
+        window.location.href = '/'
+      })
+  }
+
   return (
     <div>
       <h1>Product information</h1>
@@ -6,6 +20,11 @@ export function ProductsShow(props) {
       <p>Url: {props.product.url}</p>
       <p>Width: {props.product.width}</p>
       <p>Height: {props.product.height}</p>
+      <form onSubmit={submit}>
+        <p>Quantity: <input type="text" name="quantity" /></p>
+        <p><input type="hidden" name="product_id" defaultValue={props.product.id} /></p>
+        <input type="submit" value="add to cart" />
+      </form>
     </div>
   );
 }
